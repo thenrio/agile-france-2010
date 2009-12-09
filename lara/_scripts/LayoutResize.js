ca65f6036ea5cde6914e3e8175637c0e11683f67
Pascal PRATMARTY <pascal@pascal-2.local>
2009-12-09 08:57:57 +0100

    added lara version of the site (don't know how to merge).)

---
+++ lara/_scripts/LayoutResize.js
@@ -0,0 +1,45 @@
+﻿if (!com_joemarini) var com_joemarini = {};
+else if (com_joemarini && typeof (com_joemarini) != "object")
+    throw new Error("com_joemarini is not an Object type");
+
+com_joemarini.ADAPTIVE_LAYOUT = {
+
+    gaScrnSizes: new Array(),
+
+    initSizes: function() {
+        this.gaScrnSizes.push({ clMinW: 800, clMaxW: -1, cls: 'bodyLarge' });
+        this.gaScrnSizes.push({ clMinW: 400, clMaxW: 800, cls: 'bodyMed' });
+        this.gaScrnSizes.push({ clMinW: -1, clMaxW: 400, cls: 'bodySmall' });
+    },
+
+    setStyleForSize: function() {
+        var clientW = document.documentElement.clientWidth;
+        for (i = 0; i < this.gaScrnSizes.length; i++) {
+            var sizeObj = this.gaScrnSizes[i];
+            if (sizeObj.clMinW == -1) {
+                if (clientW <= sizeObj.clMaxW) {
+                    document.body.className = sizeObj.cls;
+                    return;
+                }
+            }
+            if (sizeObj.clMaxW == -1) {
+                if (clientW > sizeObj.clMinW) {
+                    document.body.className = sizeObj.cls;
+                    return;
+                }
+            }
+            if (clientW > sizeObj.clMinW && clientW <= sizeObj.clMaxW) {
+                document.body.className = sizeObj.cls;
+                return;
+            }
+        }
+    }
+};
+
+com_joemarini.EVENTS.addEventHandler(window, "load", function() {
+    com_joemarini.ADAPTIVE_LAYOUT.initSizes();
+    com_joemarini.ADAPTIVE_LAYOUT.setStyleForSize();
+    },false);
+
+com_joemarini.EVENTS.addEventHandler(window, "resize", function() {
+    com_joemarini.ADAPTIVE_LAYOUT.setStyleForSize();}, false);
---
+++ lara/_scripts/documentProtocol.js
@@ -0,0 +1,9 @@
+/*--Google analytics--*/
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+/*-- Page Tracker--*/
+try {
+var pageTracker = _gat._getTracker("UA-8623406-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
